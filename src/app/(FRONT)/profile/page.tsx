3aa33777e905812216b2e6ac5eb889dda5e55705
sanatan-dive <sanatansharma352@gmail.com>
2024-12-08 00:35:25 +0530

    fixed one thing which was effecting other thing

--- src/app/(FRONT)/profile/page.tsx
+++ src/app/(FRONT)/profile/page.tsx
@@ -71,7 +71,10 @@ const Profile = () => {
   };
 
   // Handle loading state
-  if (loading) return <Loading />;
+  if (loading) return
+  <div className="flex items-center justify-center min-h-screen">
+<Loading />;
+  </div> 
 
   
 
